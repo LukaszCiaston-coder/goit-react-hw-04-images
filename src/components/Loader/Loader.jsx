f2ad215bf8338f955cce39b20f70c53beefc6eee
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-07-25 10:59:57 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
 import styles from './Loader.module.css';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className={styles.loader}>
-        <div className={styles.spinner}></div>
-      </div>
-    );
-  }
-}
+const Loader = () => {
+  return (
+    <div className={styles.loader}>
+      <div className={styles.spinner}></div>
+    </div>
+  );
+};
 
 export default Loader;
