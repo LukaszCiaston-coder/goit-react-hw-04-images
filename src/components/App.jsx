80f64306747cf43f0aa2e29b9e2de312fbb60660
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-07-25 11:56:22 +0200

    add notiflix end ref a code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,106 +1,91 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import Loader from './Loader/Loader';
 import Modal from './Modal/Modal';
+import Notiflix from 'notiflix';
 
 const API_KEY = '36779044-50439618c93dcb9f395d82e01';
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    isLoading: false,
-    isLoadingMore: false,
-    selectedImage: null,
+
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleSearch = newQuery => {
+    setQuery(newQuery);
+    setImages([]);
+    setPage(1);
   };
 
-  handleSearch = newQuery => {
-    this.setState(
-      {
-        query: newQuery,
-        images: [],
-        page: 1,
-      },
-      this.fetchImages
-    );
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+    setIsLoadingMore(true);
   };
 
-  handleLoadMore = () => {
-    this.setState(
-      prevState => ({
-        page: prevState.page + 1,
-        isLoadingMore: true,
-      }),
-      this.fetchImages
-    );
+  const handleItemClick = image => {
+    setSelectedImage(image);
   };
 
-  handleItemClick = image => {
-    this.setState({
-      selectedImage: image,
-    });
+  const handleCloseModal = () => {
+    setSelectedImage(null);
   };
 
-  handleCloseModal = () => {
-    this.setState({
-      selectedImage: null,
-    });
-  };
-
-  fetchImages = async () => {
-    const { query, page } = this.state;
-
+  const fetchImages = async () => {
     if (!query) return;
 
-    this.setState({
-      isLoading: true,
-    });
+    setIsLoading(true);
 
     try {
       const response = await axios.get(
         `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
       );
 
-      this.setState(prevState => ({
-        images: [...prevState.images, ...response.data.hits],
-        isLoading: false,
-      }));
+      if (response.data.hits.length === 0 && page === 1) {
+        Notiflix.Notify.warning('No images found for the searched query.');
+      } else if (response.data.hits.length === 0 && page > 1) {
+        Notiflix.Notify.info('No more images to load.');
+      } else {
+        setImages(prevImages => [...prevImages, ...response.data.hits]);
+      }
+
+      setIsLoading(false);
+      setIsLoadingMore(false);
     } catch (error) {
       console.error('Error fetching images:', error);
-      this.setState({
-        isLoading: false,
-      });
+      setIsLoading(false);
+      setIsLoadingMore(false);
     }
   };
 
-  render() {
-    const { images, isLoading, selectedImage } = this.state;
+  useEffect(() => {
+    fetchImages();
+  }, [query, page]);
 
-    return (
-      <div className="app">
-        <Searchbar onSubmit={this.handleSearch} />
+  return (
+    <div className="app">
+      <Searchbar onSubmit={handleSearch} />
 
-        {isLoading && <Loader />}
+      {isLoading && <Loader />}
 
-        {images.length > 0 && (
-          <ImageGallery images={images} onItemClick={this.handleItemClick} />
-        )}
+      {images.length > 0 && (
+        <ImageGallery images={images} onItemClick={handleItemClick} />
+      )}
 
-        {images.length > 0 && !isLoading && (
-          <Button onClick={this.handleLoadMore} />
-        )}
+      {images.length > 0 && !isLoading && <Button onClick={handleLoadMore} />}
 
-        {selectedImage && (
-          <Modal image={selectedImage} onClose={this.handleCloseModal} />
-        )}
+      {selectedImage && (
+        <Modal image={selectedImage} onClose={handleCloseModal} />
+      )}
 
-        {/* Render other components */}
-      </div>
-    );
-  }
-}
+      {/* Render other components */}
+    </div>
+  );
+};
 
 export default App;
